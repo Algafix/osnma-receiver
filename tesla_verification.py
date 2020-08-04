4e0bc0fb50c3ac634b9d857ad49be5744b21d385
Aleix <algafix@gmail.com>
2020-08-04 14:45:42 +0200

    Added comments

--- tesla_verification.py
+++ tesla_verification.py
@@ -5,30 +5,37 @@ from test_data import *
 def key_calculation(key, GST, alpha, KS):
     m = hashlib.sha256()
     m.update(key + GST + alpha)
-    return bytearray.fromhex(m.hexdigest())[:KS//8]
+    return m.digest()[:KS//8]
 
+# Initialization of common values for the chain verification
 
 key_chain = TESTVECTOR['keychain']
 KS = TESTVECTOR['KS']
 TOW_S = TESTVECTOR['TOWS']
 alpha = bytearray.fromhex(TESTVECTOR['alpha'])
 
+# The last appended key in the chain is checked agaist the previous ones
 
-previous_key = bytearray.fromhex(key_chain[-1]['KEY'])
+last_key = bytearray.fromhex(key_chain[-1]['KEY'])
 
 for key_info in reversed(key_chain[:-1]):
 
+    # Extraction of values to improve readability
     WN = key_info['WN']
     TOW = key_info['TOW']
-    GST = bytearray.fromhex(format(WN<<TOW_S|TOW,'x'))
     current_key = bytearray.fromhex(key_info['KEY'])
-    computed_key = key_calculation(previous_key,GST,alpha,KS)
+
+    # In the test data the GST 32 bits register is fragmented onto WN and TOW,
+    # so it's necessary to re-unite it by bit alignment.
+    GST = bytearray.fromhex(format(WN<<TOW_S|TOW,'x'))
+    
+    computed_key = key_calculation(last_key,GST,alpha,KS)
 
     print('\n==========================================')
     print('Current key: ' + current_key.hex())
     print('Computed key: ' + computed_key.hex())
     if (current_key == computed_key):
-        previous_key = current_key
+        last_key = current_key
         print('\t\033[32m Same Key! \033[m')
         if(key_info['ID'] == 0):
             print('\n\t\033[1m\033[30m\033[42m Chain verifiyed! \033[m')
